d6273fc98015e5ec753ee040f120bf60f3b44a7c
JaydenHoeve <70574829+JaydenHoeve@users.noreply.github.com>
2020-09-15 14:18:56 +0200

    Add files via upload
    Dit is de phyton IDLE opdracht van de 1ste week

---
+++ JA.py
@@ -0,0 +1,14 @@
+# variablen
+# input()
+# print()
+
+# doel: Voornaam en achternaam kunnen invoeren. en het progamma weergeeft de volledig naam
+
+print("Wat is jouw voornaam?")
+voornaam = input()
+
+print("wat is jouw achternaam")
+achternaam = input()
+
+print("Jouw volledige naam is:")
+print(voornaam + " " + achternaam)
