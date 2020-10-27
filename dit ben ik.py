6e55ad3fc686edc21cf8053e612beb7255a3fa03
JaydenHoeve <70574829+JaydenHoeve@users.noreply.github.com>
2020-10-27 15:23:21 +0100

    Dit is de opdracht ditbenik.py
    Dit is de opdracht ditbenik.py

---
+++ dit ben ik.py
@@ -0,0 +1,63 @@
+print("Hallo ik ben Jayden.")
+print("Wie ben jij?")
+
+naam = input()
+print("Hallo,", naam)
+print("Om mij beter te leren kennen heb ik wat vragen voor je die je kan beantwoorden met A,B of C.")
+
+
+print("Waar woon ik?")
+
+print("A: Amsterdam")
+print("B: Zaandam")
+print("C: Hoorn")
+
+answer1 = input()
+
+if answer1 == "A":
+        print("Sorry dit antwoord is fout maar ik woon wel in Zaandam.")
+
+if answer1 == "B":
+        print("Goedzo dit antwoord is goed we gaan door naar de volgende vraag")
+
+if answer1 == "C":
+        print("Sorry dit antwoord is fout maar ik woon wel in Zaandam.")
+
+
+answer2 = input()
+
+print("Hoe oud denk je dat ik ben?")
+
+print("A: 15")
+print("B: 16")
+print("C: 17")
+
+
+if answer2 == "A":
+        print("Sorry dit antwoord is fout maar ik ben wel 16.")
+
+if answer2 == "B":
+        print("Goedzo dit antwoord is goed we gaan door naar de volgende vraag")
+
+if answer2 == "C":
+        print("Sorry dit antwoord is fout maar ik ben wel 16.")
+
+
+answer3 = input()
+
+
+print("Wat denk je dat ik mijn vrije tijd het meeste doe?")
+
+print("A: Voetballen")
+print("B: Gamen")
+print("C: Fietsen")
+
+if answer3 == "A":
+        print("Sorry dit antwoord is fout maar ik game wel heel veel in mijn vrije tijd.")
+
+if answer3 == "B":
+        print("Goedzo dit antwoord is goed we gaan door naar de volgende vraag")
+
+if answer3 == "C":
+        print("Sorry dit antwoord is fout maar ik game wel heel veel in mijn vrije tijd.")
+
